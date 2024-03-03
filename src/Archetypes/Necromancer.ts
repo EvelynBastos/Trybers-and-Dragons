4951f9968d28f7749cab894bd3ee7f5c1a86ec7f
EvelynBastos <lynbastos@icloud.com>
2024-03-03 00:49:40 -0300

    necromancer

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Necromancer extends Archetype {
+  static _createdInstances = 0;
+  private _energyType: EnergyType;
+
+  constructor(name: string) {
+    super(name);
+    Necromancer._createdInstances += 1;
+    this._energyType = 'mana';
+  }
+  
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Necromancer._createdInstances;
+  }
+}
+
+export default Necromancer;
